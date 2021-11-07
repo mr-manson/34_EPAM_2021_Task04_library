dd9eb5d0adab1e70460623c6ba6b554554b4db60
mr-manson <60350677+mr-manson@users.noreply.github.com>
2021-11-07 22:19:45 +0300

    Add files via upload

--- src/by/epamtc/nikalaichanka/task04/library/dao/UserDAO.java
+++ src/by/epamtc/nikalaichanka/task04/library/dao/UserDAO.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 public interface UserDAO {
 
     User authorization(String login, String password) throws DAOException, FileNotFoundException;
-    User registration(User newUser) throws DAOException, FileNotFoundException ;
-    User remove(int userID) throws DAOException, FileNotFoundException ;
+    boolean registration(User newUser) throws DAOException, FileNotFoundException ;
+    boolean remove(int userID) throws DAOException, FileNotFoundException ;
 
 }
