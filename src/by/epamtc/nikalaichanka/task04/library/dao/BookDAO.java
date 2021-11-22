e6dacfa7e0a581c7bb39aa100fd2fa9270c39d03
mr-manson <60350677+mr-manson@users.noreply.github.com>
2021-11-22 13:32:03 +0300

    Delete BookDAO.java

--- src/by/epamtc/nikalaichanka/task04/library/dao/BookDAO.java
+++
@@ -1,8 +0,0 @@
-package by.epamtc.nikalaichanka.task04.library.dao;
-
-import java.io.FileNotFoundException;
-
-public interface BookDAO {
-
-    boolean addBook(String bookAuthor, String bookTitle) throws DAOException, FileNotFoundException;
-}
